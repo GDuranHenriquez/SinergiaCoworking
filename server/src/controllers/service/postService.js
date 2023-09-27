8f82142ba98c05689caa37ea0e5ba4606dd71aee
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-27 16:08:05 -0300

    add basic controllers. Fran

---
+++ server/src/controllers/service/postService.js
@@ -0,0 +1,19 @@
+const {Service} = require('../../db')
+
+const postServices = async (req, res) => {
+    try {
+        const {name} = req.body
+        if(!name){
+            return res.status(401).json({error: 'The name must be provided'})
+        }
+        const [service, created] = await Service.findOrCreate({where: {name}})
+        if(created){
+            return res.status(403).json({error: 'The name provided is already registered'})
+        }
+        return res.status(200).json(service)
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
+
+module.exports = postServices
