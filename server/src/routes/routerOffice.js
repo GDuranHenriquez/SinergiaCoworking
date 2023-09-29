91b9bc6a6286f9269c7eed01c495fc0fdc5a14fa
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-29 06:16:46 -0300

    add route for availabilityCheck. Fran

--- server/src/routes/routerOffice.js
+++ server/src/routes/routerOffice.js
@@ -3,12 +3,14 @@ const deleteOffice = require('../controllers/office/deleteOffice')
 const getOffices = require('../controllers/office/getOffices')
 const postOffice = require('../controllers/office/postOffice')
 const getOfficeById = require('../controllers/office/getOfficeById')
+const availabilityCheck = require('../controllers/office/availabilityCheck')
 
 
 const routerOffice = Router()
 
 routerOffice.get("/", getOffices)
 routerOffice.get("/:id", getOfficeById)
+routerOffice.get("/check-date", availabilityCheck)
 routerOffice.post("/", postOffice)
 routerOffice.delete("/:id", deleteOffice)
 
