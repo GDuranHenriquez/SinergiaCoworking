efee32438d0b8645e74ac8b676767062cbd9923a
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-29 14:17:44 -0300

    add reservation relation to getOfficeById. Fran

--- server/src/controllers/office/getOfficeById.js
+++ server/src/controllers/office/getOfficeById.js
@@ -1,4 +1,4 @@
-const {Office, Service} = require('../../db')
+const {Category, Office, OfficeImage, Score, Service, Reservation} = require('../../db')
 
 
 const getOfficeById = async (req, res) => {
@@ -6,14 +6,16 @@ const getOfficeById = async (req, res) => {
         const { id } = req.params
         const office = await Office.findOne({where: {id}, 
             include: [
-                {model: Service, through: {attributes: []}}
+                {model: Service, through: {attributes: []}},
+                {model: Category, as: 'office_category'},
+                {model: OfficeImage, as: 'office_officeImage'},
+                {model: Reservation, as: 'office_reservation'}
             ]});
 
-        if(office) {
-            return res.status(200).json(office);
-        } else {
-            return res.status(404).json({ message: "Office not found" });
+        if(!office) {
+            return res.status(404).json({ message: "No se encontró la oficina" });
         }
+        return res.status(200).json(office);
     } catch (error) {
         return res.status(500).json({error: error.message});
     }
