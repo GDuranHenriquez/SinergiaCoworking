b710229558f8941f4b2d216b37775b94b34f4877
agradi20 <agradimanuel@gmail.com>
2023-10-03 21:39:17 -0300

    Agregando Controllers. Manu.A

--- server/src/routes/routerBuilding.js
+++ server/src/routes/routerBuilding.js
@@ -2,10 +2,12 @@ const {Router} = require('express')
 const postBuilding = require('../controllers/building/postBuilding')
 const getBuildings = require('../controllers/building/getBuildings')
 const deleteBuilding = require('../controllers/building/deleteBuilding')
+const getBuildingById = require("../controllers/building/getBuildingById")
 
 const routerBuilding = Router()
 
 routerBuilding.get("/", getBuildings)
+routerBuilding.get("/:id", getBuildingById)
 routerBuilding.post("/", postBuilding)
 routerBuilding.delete("/:id", deleteBuilding)
 
