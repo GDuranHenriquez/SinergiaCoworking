a36efaa1421ccefaccafd8e3c2569aaeaa03cb11
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-10-11 19:14:40 -0400

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
+  build: { chunkSizeWarningLimit: 2000,
+    /* rollupOptions: {
         output:{
             manualChunks(id) {
                 if (id.includes('node_modules')) {
@@ -13,6 +13,6 @@ export default defineConfig({
                 }
             }
         }
-    }
+    } */
 }
 })
