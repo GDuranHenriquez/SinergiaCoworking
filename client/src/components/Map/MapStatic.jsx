0b10e20b4b001bcd152c1370178923072b726108
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-10-18 18:18:20 -0300

    marker static. Fran

--- client/src/components/Map/MapStatic.jsx
+++ client/src/components/Map/MapStatic.jsx
@@ -54,7 +54,7 @@ function MapStatic({position = [-32.81513813534083, -62.158580722506244], zoom =
         iconCreateFunction={createClusterCustomIcon}
       >
         {markers.length > 0 ? markers.map((marker) => (
-          <Marker position={marker.geocode} icon={customIcon} draggable={true} autoPan={true}>
+          <Marker position={marker.geocode} icon={customIcon} draggable={false} autoPan={true}>
             <Popup>
             <div style={{width: '200px', height:'250px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <img style={{width: "10rem", height: "10rem"}} src={marker.popUpImg} />
