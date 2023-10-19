615594b1b6d4166ffa9aa9cc5dddf1dbf70a443b
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-10-18 22:10:08 -0400

    develop

--- server/src/controllers/protectedUser/getDataUser.js
+++ server/src/controllers/protectedUser/getDataUser.js
@@ -4,11 +4,10 @@ async function getDataUserClient(req, res){
   try {
     const user = req.user;
     const newUser = await User.findOne({ where: { id : user.id } });
-    if(user === null){
+    if(newUser === null){
       return res.status(403).json({error: 'Unregistered user, please create an account, go to create an account to register. '});
     }
     var data = newUser.dataValues;  
-    console.log(data)
     data = generateInfo(data)
    return res.status(200).json(data);   
   } catch (error) {
